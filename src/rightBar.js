6eab547666489d2cc27d1f571c4a7a393bf0a637
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-09-24 22:06:18 +0300

    small improvements

--- src/rightBar.js
+++ src/rightBar.js
@@ -3,20 +3,20 @@ import React from "react";
 function RightBar({reset, theme, setTheme}) {
   return (
     <div className="aside aside-right">
-        <div class="themes">
-          <div class="row">
+        <div className="themes">
+          <div className="row">
             <input id="check-0" type="radio" checked={theme===0} onChange={_=>setTheme(0)} />
-            <label for="check-0" class="cell theme-0"></label>
+            <label htmlFor="check-0" className="cell theme-0"></label>
             
             <input id="check-1" type="radio" checked={theme===1} onChange={_=>setTheme(1)} />
-            <label for="check-1" class="cell theme-1"></label>
+            <label htmlFor="check-1" className="cell theme-1"></label>
           </div>
-          <div class="row">
+          <div className="row">
             <input id="check-2" type="radio" checked={theme===2} onChange={_=>setTheme(2)} />
-            <label for="check-2" class="cell theme-2"></label>
+            <label htmlFor="check-2" className="cell theme-2"></label>
             
             <input id="check-3" type="radio" checked={theme===3} onChange={_=>setTheme(3)} />
-            <label for="check-3" class="cell theme-3"></label>
+            <label htmlFor="check-3" className="cell theme-3"></label>
           </div>
         </div>
 
